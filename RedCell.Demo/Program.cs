ce2381a64e705b376c6c4ec235740e462854976a
RMLehnemann <rodrigo.medeiros.email@gmail.com>
2020-01-25 20:15:13 -0300

    feat(clean code): removed comentaries.

--- RedCell.Demo/Program.cs
+++ RedCell.Demo/Program.cs
@@ -6,9 +6,6 @@ namespace RedCell.Demo
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main()
         {
             Log.Console = true;
