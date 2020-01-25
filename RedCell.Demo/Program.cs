0ef842208a3b7accb9507f2fbe4605f8ea62648a
RMLehnemann <rodrigo.medeiros.email@gmail.com>
2020-01-25 11:22:44 -0300

    feat(demo)

---
+++ RedCell.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿using System;
+
+namespace RedCell.Diagnostics.Update.Demo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            Log.Console = true;
+            Console.WriteLine("Hello World");
+            var updater = new Updater();
+            updater.StartMonitoring();
+        }
+    }
+}
