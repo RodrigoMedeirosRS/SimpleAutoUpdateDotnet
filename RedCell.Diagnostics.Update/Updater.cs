b224452d770122fd56e2a3fc654386b058880762
RMLehnemann <rodrigo.medeiros.email@gmail.com>
2020-01-25 20:13:53 -0300

    feat(identation and clean code): removed comentaries and re-idented code.

--- RedCell.Diagnostics.Update/Updater.cs
+++ RedCell.Diagnostics.Update/Updater.cs
@@ -11,49 +11,22 @@ namespace RedCell.Diagnostics.Update
 {
     public class Updater
     {
-        #region Constants
-        /// <summary>
-        /// The default check interval.
-        /// </summary>
         public const int DefaultCheckInterval = 900; // 900s = 15m
-
-        /// <summary>
-        /// The first check delay.
-        /// </summary>
         public const int FirstCheckDelay = 15;
-
-        /// <summary>
-        /// The default configuration file.
-        /// </summary>
         public const string DefaultConfigFile = "update.xml";
-
-        /// <summary>
-        /// The name of the derectory
-        /// </summary>
         public const string WorkPath = "Document";
-        #endregion
 
-        #region Fields
         private Timer _timer;
         private volatile bool _updating;
         private readonly Manifest _localConfig;
         private Manifest _remoteConfig;
         private readonly FileInfo _localConfigFile;
-        #endregion
 
-        #region Initialization
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Update"/> class
-        /// </summary>
         public Updater() : this(new FileInfo(DefaultConfigFile))
         {
 
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Update"/> class
-        /// </summary>
-        /// <param name="configFile"></param>
         public Updater(FileInfo configFile)
         {
             Log.Debug = true;
@@ -63,7 +36,7 @@ namespace RedCell.Diagnostics.Update
             Log.Write("Loaded.");
             Log.Write("Initializing using file '{0}'.", configFile.FullName);
 
-            if (!configFile.Exists) // nếu configFile không tồn tại, thông báo và dừng lại
+            if (!configFile.Exists)
             {
                 Log.Write("Config file {0} does not exist, stopping.", configFile.Name);
                 return;
@@ -73,12 +46,7 @@ namespace RedCell.Diagnostics.Update
             Console.WriteLine("Information of data:\n" + data);
             this._localConfig = new Manifest(data);
         }
-        #endregion
 
-        #region Methods
-        /// <summary>
-        /// Starting monitoring.
-        /// </summary>
         public void StartMonitoring()
         {
             Log.Write("Starting monitoring in {0}s...", this._localConfig.CheckInterval);
@@ -86,9 +54,7 @@ namespace RedCell.Diagnostics.Update
             _timer = new Timer(Check, null, 5000, this._localConfig.CheckInterval * 1000);
             Log.Write("Already.");
         }
-        /// <summary>
-        /// Stopping monitoring.
-        /// </summary>
+
         public void StopMonitoring()
         {
             Log.Write("Stopping monitoring.");
@@ -101,53 +67,37 @@ namespace RedCell.Diagnostics.Update
             _timer.Dispose();
         }
 
-        /// <summary>
-        /// Check Remote Configuration
-        /// </summary>
-        /// <param name="state"></param>
         private void Check(Object state)
         {
             Log.Write("Checking...");
 
-            if (_updating) // nếu _updating != null, việc kiểm tra cập nhật đã hoàn thành.
+            if (_updating)
             {
                 Log.Write("Updater is already updating.");
                 Log.Write("Check ending.");
             }
 
-            // Lấy đường dẫn chứa file xml
             var remoteUri = new Uri(this._localConfig.RemoteConfigUri);
-            // In ra đường dẫn phiên bản hiện tại đang sử dụng
             Log.Write("Fetching local file: '{0}'.", remoteUri.AbsoluteUri);
-
-            // Khởi tạo một đối tượng http kiểu Fetch
             var http = new Fetch { Retries = 5, RetrySleep = 30000, Timeout = 30000 };
-            // Tải về đường đẫn chính xác URI
             http.Load(remoteUri.AbsoluteUri);
 
-            if (!http.Success) // nếu http khởi tạo không thành công, thông báo lỗi xảy ra
+            if (!http.Success)
             {
                 Log.Write("Fetch error: {0}", http.Response.StatusDescription);
                 this._remoteConfig = null;
                 return;
             }
-
-            // Lấy thông tin dữ liệu phản hồi từ http và định dạng font chữ cho dữ liệu 
             string data = Encoding.UTF8.GetString(http.ResponseData);
-
-            // Khởi tạo đối tượng kiểu Manifest, lưu trữ thông tin của file xml chuẩn bị cập nhật
             this._remoteConfig = new Manifest(data);
 
-            // Kiểm tra các trường hợp 
-
-            if (this._remoteConfig == null) // nếu dữ liệu KHÔNG được tạo thành công, thông báo
+            if (this._remoteConfig == null)
             {
                 Log.Write("Data is not found!, stopping...");
                 Log.Write("Check ending.");
                 return;
             }
 
-            // Nếu mã bảo mật của phiên bản mới không giống phiên bản đang sử dụng, thông báo
             if (this._localConfig.SecurityToken != this._remoteConfig.SecurityToken)
             {
                 Log.Write("Security token mismatch, stopping...");
@@ -155,51 +105,39 @@ namespace RedCell.Diagnostics.Update
                 return;
             }
 
-            // In ra thông tin số phiên bản của các phiên bản cũ và mới
             Log.Write("Remote config is valid.");
             Log.Write("Local version is {0}", _localConfig.Version);
             Log.Write("Remote version is {0}", _remoteConfig.Version);
 
-            // Kiểm tra số phiên bản hợp lệ không?
-            // Phiên bản xem xét cập nhật có số phiên bản thấp hơn, kết thúc kiểm tra, KHÔNG cập nhật
             if (this._remoteConfig.Version < this._localConfig.Version)
             {
                 Log.Write("Remote version is older. That's weird.");
                 Log.Write("Check ending.");
                 return;
             }
-            // 2 số phiên bản là giống nhau, kết thúc kiểm tra, KHÔNG cập nhật
             if (this._remoteConfig.Version == this._localConfig.Version)
             {
                 Log.Write("Versions are the same.");
                 Log.Write("Check ending.");
                 return;
             }
-            // ngược lại nếu, phiên bản xem xét cập nhật là mới hơn, cho phép cập nhật
             Log.Write("Remote version is newer. Updating.");
             _updating = true;
             Update();
 
-            // Kết thúc việc kiểm tra cập nhật, đã chấp nhận cập nhật
             _updating = false;
             Log.Write("Check ending.");
             Console.ReadKey();
         }
-
-        /// <summary>
-        /// Update program
-        /// </summary>
         private void Update()
         {
-            // Thông báo số file cần cập nhật và in ra tên file đó.
             Log.Write("Updating '{0}' files.", this._remoteConfig.Payloads.Length);
             foreach (string str in this._remoteConfig.Payloads)
             {
                 Console.WriteLine("Remote Payloads: " + str);
             }
 
-            // Xóa bỏ thư mục nếu nó đã tồn tại
-            if (Directory.Exists(WorkPath)) // nếu tồn tại, cảnh báo và xóa nó đi.
+            if (Directory.Exists(WorkPath))
             {
                 Log.Write("WARNING: Work directory already exists.");
 
@@ -213,35 +151,26 @@ namespace RedCell.Diagnostics.Update
                     return;
                 }
             }
-            // Tạo thư mục với tên thư mục là 'Document'
             Directory.CreateDirectory(WorkPath);
 
-            // Tải về các file từ Manifest
             foreach (string update in this._remoteConfig.Payloads)
             {
-                // Thông báo những file được tải về
                 Log.Write("Fetching '{0}'.", update);
-                // Tạo đường dẫn đến file cần tải.
                 var url = this._remoteConfig.BaseUri + update;
-                // Lấy dữ liệu từ file cần tải về.
                 var file = Fetch.Get(url);
 
-                if (file == null) // nếu lấy dữ liệu không thành công, thông báo và kết thúc.
+                if (file == null)
                 {
                     Log.Write("Fetch failed.");
                     return;
                 }
 
-                // Tạo đường dẫn để lưu file khi tải file về
                 var info = new FileInfo(Path.Combine(WorkPath, update));
-                // In ra đường dẫn lưu file
                 Console.WriteLine("File is saved in: {0}", info.FullName);
 
-                // Tạo thư mục để lưu trữ file với đường dẫn đã có
                 Directory.CreateDirectory(info.DirectoryName);
                 File.WriteAllBytes(Path.Combine(WorkPath, update), file);
 
-                // Nếu file có dạng .zip, không mở, tải về cả .zip
                 if (Regex.IsMatch(update, @"\.zip"))
                 {
                     try
@@ -259,45 +188,32 @@ namespace RedCell.Diagnostics.Update
                 }
             }
 
-
-            // Thay đổi thực thi nếu nó cần được ghi đè
             Process thisprocess = Process.GetCurrentProcess();
             string me = thisprocess.MainModule.FileName;
 
-            // In ra đường dẫn đến chương trình đang thực thi.
             Console.WriteLine("Path: " + me);
-            string bak = me + ".bak"; // thêm đuôi .bak
+            string bak = me + ".bak";
             Log.Write("Renaming running process to '{0}'.", bak);
 
-            if (File.Exists(bak)) // nếu đã tồn tại file.bak, xóa nó đi.
+            if (File.Exists(bak))
                 File.Delete(bak);
             File.Move(me, bak);
             File.Copy(bak, me);
 
-            // Ghi dữ liệu lên Manifest mới.
             _remoteConfig.Write(Path.Combine(WorkPath, _localConfigFile.Name));
 
-            // Sao chép mọi thứ.
-            // Tạo thư mục với tên 'Document'.
             var directory = new DirectoryInfo(WorkPath);
-            // Lấy về các file.
             var files = directory.GetFiles("*.*", SearchOption.AllDirectories);
             foreach (FileInfo file in files)
             {
-                // Tạo đích đến cho các file.
                 string destination = file.FullName.Replace(directory.FullName + @"\", "");
-                // In ra những file cần cài đặt.
                 Log.Write("installing file '{0}'.", destination);
-                // Tạo tất cả các đường dẫn với tên đã xác định.
                 Directory.CreateDirectory(new FileInfo(destination).DirectoryName);
                 file.CopyTo(destination, true);
             }
-
-            // Xóa thư mục 'Document'
             Log.Write("Deleting work directory.");
+            
             Directory.Delete(WorkPath, true);
-
-            // Khởi động lại
             Log.Write("Spawning new process.");
             var spawn = Process.Start(me);
 
@@ -311,6 +227,5 @@ namespace RedCell.Diagnostics.Update
             thisprocess.Dispose();
 
         }
-        #endregion
     }
 }
--- RedCell.Net/FetchStream.cs
+++ RedCell.Net/FetchStream.cs
@@ -5,73 +5,40 @@ using System.Text;
 
 namespace RedCell.Net
 {
-    /// <summary>
-    /// Fetches streamed content.
-    /// </summary>
     public class FetchStream
     {
-        #region Properties
-        /// <summary>
-        /// Gets the response.
-        /// </summary>
         public HttpWebResponse Response { get; private set; }
-
-        /// <summary>
-        /// Gets the response data.
-        /// </summary>
         public byte[] ResponseData { get; private set; }
-        #endregion
-
-        #region Methods
-        /// <summary>
-        /// Gets the specified URL.
-        /// </summary>
-        /// <param name="url"></param>
         public void Load(string url)
         {
             try
             {
-                // Tạo một HttpWebRequest đến URL được đề cập.
                 var req = HttpWebRequest.Create(url) as HttpWebRequest;
-                // Cho phép req có thể chuyển hướng đến một vị trí mới.
                 req.AllowAutoRedirect = false;
-
-                // Nhận những phản hồi trả về từ tài nguyên trên web.
                 Response = req.GetResponse() as HttpWebResponse;
-                // Kiểm tra trạng thái của Response.
                 switch (Response.StatusCode)
                 {
-                    // Giá trị mã trạng thái cho HTTP không tìm thấy.
                     case HttpStatusCode.Found:
-                        {
-                            // Đây là chuyển hướng đến một trang lỗi, bỏ qua...
-                            Console.WriteLine("Found (302), ignoring ");
-                            break;
-                        }
-
-                    // Giá trị mã trạng thái cho HTTP tìm thấy.
+                    {
+                        Console.WriteLine("Found (302), ignoring ");
+                        break;
+                    }
                     case HttpStatusCode.OK:
+                    {
+                        using (var sr = Response.GetResponseStream())
+                        using (var ms = new MemoryStream())
                         {
-                            // Đây là trang khả dụng.
-
-                            // Nhận các dòng tin từ máy chủ gửi về.
-                            using (var sr = Response.GetResponseStream())
-                            using (var ms = new MemoryStream())
-                            {
-                                for (int b; (b = sr.ReadByte()) != -1;)
-                                    ms.WriteByte((byte)b);
-                                // Lưu trữ những dòng tin được gửi về.
-                                ResponseData = ms.ToArray();
-                            }
-                            break;
+                            for (int b; (b = sr.ReadByte()) != -1;)
+                                ms.WriteByte((byte)b);
+                            ResponseData = ms.ToArray();
                         }
-
+                        break;
+                    }
                     default:
-                        {
-                            // Trường hợp không mong đợi.
-                            Console.WriteLine(Response.StatusCode);
-                            break;
-                        }
+                    {
+                        Console.WriteLine(Response.StatusCode);
+                        break;
+                    }
                 }
             }
             catch (WebException ex)
@@ -80,23 +47,12 @@ namespace RedCell.Net
                 Response = ex.Response as HttpWebResponse;
             }
         }
-
-        /// <summary>
-        /// Gets the specified URL.
-        /// </summary>
-        /// <param name="url">The URL.</param>
-        /// <returns></returns>
         public static byte[] Get(string url)
         {
             var f = new Fetch();
             f.Load(url);
             return f.ResponseData;
         }
-
-        /// <summary>
-        /// Gets the string.
-        /// </summary>
-        /// <returns></returns>
         public string GetString()
         {
             var encoder = string.IsNullOrEmpty(Response.ContentEncoding) ?
@@ -105,6 +61,5 @@ namespace RedCell.Net
                 return string.Empty;
             return encoder.GetString(ResponseData);
         }
-        #endregion
     }
 }
